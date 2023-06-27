703d490e626c1a096a6490d913ecda8ce3000349
papachio <john.papachristoss@gmail.com>
2023-06-27 18:06:01 +0300

    Extra feature added, to display all the users.

--- studentfrontend/src/components/ApplicationTextField.js
+++ studentfrontend/src/components/ApplicationTextField.js
@@ -1,14 +1,16 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
+import Paper from '@mui/material/Paper';
 
 export default function BasicTextFields() {
 	const[name,setName]=useState('')
 	const[address,setAddress]=useState('')
-	
+	const[students,setStudents]=useState([])
+	const paperStyle={padding:'50px 20px', width:600,margin:"20px auto"}
 	 function handleClick(e) {
 	    e.preventDefault()
 	    
@@ -22,6 +24,17 @@ export default function BasicTextFields() {
 		console.log("New student added.");
 	})
 	  }
+	  
+	  
+	 useEffect(()=>{
+		  fetch("http://localhost:8080/student/get")
+		  .then(res=>res.json())
+		  .then((result)=>{
+		    setStudents(result);
+		  }
+		)
+},[])
+	
   return (	
     <Box
       component="form"
@@ -50,6 +63,19 @@ export default function BasicTextFields() {
     <Button variant="contained" onClick={handleClick}>
         Send
       </Button>
+      
+      <Paper elevation={3} style={paperStyle}>
+      <h2>STUDENTS</h2>
+      	{students.map(student=>(
+		<Paper elevation={6} style={{margin:"10px",padding:"15px",textAlign:"left"}} key={student.id}>
+			Id:{student.id}<br></br>
+			Name:{student.name}<br></br>
+			Address:{student.address}<br></br>
+			</Paper>
+			
+))}
+      
+      </Paper>
     </Box>
     
   );
